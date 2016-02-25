149378949c0d4358e8e68ccdf549ce33f6174d44
Derick Ostrenko <dostrenko@gmail.com>
2016-02-24 20:30:39 -0600

    re-organized folders

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
-// ************************************************
-
-// NEXUS Node Server
-//				Jesse Allison (2015)
+// ***************************************************
+//	NEXUS Node SERVER
+//	Jesse Allison (2015)
 //
 //	To Launch:
 //		NODE_ENV=production sudo node nexus-server.js
 //		(sudo is required to launch on port 80.)
+// ***************************************************
 
-// ************************************************
-
-
-	// Setup web app - using express to serve pages
+// Setup web app - using express to serve pages
 var express = require('express'),
 		sio = require('socket.io'),
 		http = require('http');
 
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/public'));
 
-	//	OSC Setup for sending (and receiving) OSC (to Max)
+//	OSC Setup for sending (and receiving) OSC (to Max)
 var osc = require('node-osc');
-		// oscServer is used for receiving osc messages (from Max)
+
+// oscServer is used for receiving osc messages (from Max)
 var oscServer = new osc.Server(7746, '167.96.127.8');
+
 oscServer.on("message", function (msg, rinfo) {
-			// console.log("OSC message:");
-			// console.log(msg);
-					// Setup messages to receive here //
+	// console.log("OSC message:");
+	// console.log(msg);
+	// Setup messages to receive here //
 	if(msg[0] = "/goToSection") {
 		currentSection = msg[1];
 		shareSection(currentSection);
 	}
 });
 
-		// oscClient is used to send osc messages (to Max)
+// oscClient is used to send osc messages (to Max)
 var oscClient = new osc.Client('167.96.127.8', 7745);
 
-
-	// server is the node server (web app via express)
-		// this code launches the server on port 80 and switches the user id away from sudo
-		// apparently this makes it more secure - if something goes awry it isn't running under the superuser.
+// server is the node server (web app via express)
+// this code launches the server on port 80 and switches the user id away from sudo
+// apparently this makes it more secure - if something goes awry it isn't running under the superuser.
 var server = http.createServer(app)
 	.listen(8000, function(err) {
 		if (err) return cb(err);
@@ -47,16 +45,16 @@ var server = http.createServer(app)
 		// Find out which user used sudo through the environment variable
 		var uid = parseInt(process.env.SUDO_UID);
 		// Set our server's uid to that user
+		
 		if (uid) process.setuid(uid);
 		console.log('Server\'s UID is now ' + process.getuid());
 	});
 
-		// start socket.io listening on the server
+// start socket.io listening on the server
 var io = sio.listen(server);
 
-
 // *********************
-	// Global Variables!
+// Global Variables!
 
 var ioClients = [];		// list of clients who have logged in.
 var currentSection = 0;		// current section.
@@ -65,9 +63,9 @@ var conrollerID;
 
 // *********************
 
-	// Respond to web sockets with socket.on
+// Respond to web sockets with socket.on
 io.sockets.on('connection', function (socket) {
-	var ioClientCounter = 0;		// Can I move this outside into global vars?
+	var ioClientCounter = 0; // Can I move this outside into global vars?
 
 	socket.on('addme', function(data) {
 		username = data.name;
@@ -89,7 +87,7 @@ io.sockets.on('connection', function (socket) {
 			ioClients.push(socket.id);
 		}
 
-		socket.username = username;  // allows the username to be retrieved anytime the socket is used
+		socket.username = username; // allows the username to be retrieved anytime the socket is used
 		// Can add any other pertinent details to the socket to be retrieved later
 		socket.userLocation = userLocation;
 		socket.userColor = userColor;
@@ -112,15 +110,15 @@ io.sockets.on('connection', function (socket) {
 		}
 	});
 
-	 socket.on('disconnect', function() {
+	socket.on('disconnect', function() {
 		// ioClients.remove(socket.id);	// FIXME: Remove client if they leave
 		io.sockets.emit('chat', 'SERVER: ' + socket.id + ' has left the building');
-	 });
+	});
 
-	 socket.on('sendchat', function(data) {
+	socket.on('sendchat', function(data) {
 		// Transmit to everyone who is connected //
 		io.sockets.emit('chat', socket.username, data);
-	 });
+	});
 
 	socket.on('tap', function(data) {
 		console.log("Data: ", data.inspect);
@@ -143,9 +141,10 @@ io.sockets.on('connection', function (socket) {
 		// TODO: Take out all the socket.broadcast.emits.
 		// socket.broadcast.emit('chat', socket.id + " : " + data, 1);
 
-	    if(io.sockets.connected[theaterID]!== null) {
-	        io.sockets.connected[theaterID].emit('itemback', {phrase: data, color: socket.userColor}, 1);
-	    }
+		if(io.sockets.connected[theaterID]!== null) {
+			io.sockets.connected[theaterID].emit('itemback', {phrase: data, color: socket.userColor}, 1);
+		}
+
 		// socket.broadcast.emit('itemback', {phrase: data, color: socket.userColor}, 1);
 		oscClient.send('/causeway/phrase/number', socket.id, data);
 	});
@@ -160,17 +159,15 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('slider', function(data) {
 		console.log("slider! " + data);
-
 		oscClient.send('/slider', socket.username, data);
 	});
-
 
 	// Return random user name
 	socket.on('getSomebody', function(data) {
 		console.log("Get Somebody! ");
 
 		var user = getNextUser();
-					// Make sure you don't get yourself
+		// Make sure you don't get yourself
 		if(user.username == socket.username || user.username == null) {
 			user = getNextUser();
 			if(user.username == socket.username || user.username == null) {
@@ -181,7 +178,6 @@ io.sockets.on('connection', function (socket) {
 		}
 	});
 
-
 	socket.on('section', function(data) {
 		console.log("Section is now: "+ data);
 		currentSection = data;
@@ -189,9 +185,8 @@ io.sockets.on('connection', function (socket) {
 	})
 
 	// *********************
-			// Functions for handling stuff
-
-			// **** SECTIONS ****
+	// Functions for handling stuff
+	// **** SECTIONS ****
 	var sectionTitles = ["Welcome", "Preface", "Section 1", "Section 2", "Section 3", 
 		"Section 4", "Section 5", "Section 6", "Section 7", "Section 8", "Section 9", 
 		"Section 10", "Section 11", "Section 12", "Section 13", "Section 14", "Section 15",  
@@ -210,7 +205,7 @@ io.sockets.on('connection', function (socket) {
 
 	};
 
-		// Section shared from Max to UIs
+	// Section shared from Max to UIs
 	shareSection = function(sect) {
 		var title = getSection(sect);
 		io.sockets.emit('setSection', sect, title);
