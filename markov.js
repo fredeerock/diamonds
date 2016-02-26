a768dd33dc2bd87ae4b657b75e8f41edfde62481
Derick Ostrenko <dostrenko@gmail.com>
2016-02-26 00:41:34 -0600

    redis insert working

--- markov.js
+++ markov.js
@@ -1,5 +1,6 @@
 var redis = require("redis"),
     client = redis.createClient();
+
 var csv = require('csv-parser');
 var fs = require('fs');
 
@@ -18,26 +19,48 @@ client.on("error", function (err) {
 //     client.quit();
 // });
 
+// var listName = "items";
+// client.ltrim(listName, -1, -2, handleTrim);
+
+// function handleTrim(){
+// 	fs.createReadStream('data/test.csv').pipe(csv()).on('data', handleRow);
+// }
+
+// function handleRow(data) {
+//     console.log('Name: %s Age: %s', data.title, data.author)
+//   	client.rpush("new1", data.title, redis.print);
+
+// }
+
+//***
+
 var listName = "items";
 
 client.ltrim(listName, -1, -2, handleTrim);
 
 function handleTrim() {
 	console.log("trimmed");
-	fs.createReadStream('data/test.csv').pipe(csv()).on('data', handleRead);
+	fs.createReadStream('data/test.csv').pipe(csv()).on('data', handleRow).on('end', handleEnd);
 }
 
-function handleRead(err, data) {
-	// console.log(data);
-	client.rpush(listName, data, handlePush);
-	console.log("pushing...");
+function handleRow(data) {
+    console.log(data.title, "by", data.author)
+	client.rpush(listName, JSON.stringify(data), redis.print);
 }
 
-function handlePush (err, push){
-	console.log('finished!', push);
+function handleEnd() {
+	console.log('file read');
 	client.llen(listName, function(err, len){console.log(len)});
+		console.log('file read');
+
+	client.lindex(listName, 2, function (err, data) {console.log(data)})
+	client.quit();
 }
 
+//***
 
-
-
+// function handlePush (err, data) {
+// 	// redis.print;
+// 	console.log("pushing... ", count);
+// 	count++;
+// }
