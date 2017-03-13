b927dba2fe47b9dd7514b214bd0497a232d0cf0f
Derick Ostrenko <dostrenko@gmail.com>
2017-03-12 23:15:01 -0500

    cleaned up loadredis

--- loadRedis.js
+++ loadRedis.js
@@ -24,35 +24,46 @@ function handleTrim() {
 	console.log("---Trimmed---");
 }
 
-// Load the data.
-fs.createReadStream(dataSet).pipe(csv()).on('data', handleRow).on('end', handleEnd);
+client.flushall( function (err, res) {
+    console.log("flush", res); // will be true if successfull
+    // Load the data.
+	fs.createReadStream(dataSet).pipe(csv()).on('data', handleRow).on('end', handleEnd);
+});
 
 // push each row into redis as a string
 function handleRow(data) {
-  console.log(data.title, "by", data.author)
+  // console.log(data.title, "by", data.author)
 
 				// ---- Load the texts into the set.
 				// Uncomment if using a list instead of sets to store the texts. //
 	// client.rpush(listName, JSON.stringify(data, escape), redis.print);
 		// Load the data as a set //
-	client.sadd(textName, JSON.stringify(data, escape));
+	// console.log(escape);
+	var jsonString = JSON.stringify(data, escape);
+	client.sadd(textName, jsonString);
+	console.log(jsonString)
 
 	// remove line breaks and other escaped formatting
 	function escape (key, val) {
-	    if (typeof(val)!="string") return val;
+	    if (typeof(val)!="string"){ 
+	    	return val;
+	    }
+	    
 	    return val
-	      // .replace(/[\"]/g, '\\"')
-	      // .replace(/[\\]/g, '\\\\')
-	      .replace(/[\']/g, '')
-	      .replace(/[\"]/g, '')
-	      .replace(/[\,]/g, '') // TODO: Commas are still messing things up!
-	      .replace(/[\/]/g, '\\/')
-	      .replace(/[\b]/g, '\\b')
-	      .replace(/[\f]/g, '\\f')
-	      .replace(/[\n]/g, '\\n')
-	      .replace(/[\r]/g, ' ')
-	      .replace(/[\t]/g, '\\t')
-				// .replace(/[\r]/g, '\\r')
+		// .replace(/[\"]/g, '\\"')
+		// .replace(/[\\]/g, '\\\\')
+		.replace(/[\']/g, '')
+		.replace(/[\"]/g, '')
+		.replace(/[,]/g, '') // TODO: Commas are still messing things up!
+		.replace(/[\\]/g, '')
+		.replace(/[\/]/g, '\\/')
+		.replace(/[\b]/g, '\\b')
+		.replace(/[\f]/g, '\\f')
+		.replace(/[Í]/g, '')
+		.replace(/[\n]/g, '\\n')
+		.replace(/[\r]/g, ' ')
+		.replace(/[\t]/g, '\\t')
+		// .replace(/[\r]/g, '\\r')
 	    ;
 	}
 }
@@ -60,6 +71,9 @@ function handleRow(data) {
 // when done reading the file display total number of items and quit redis connection
 function handleEnd() {
 	console.log('---Done reading file---');
+	client.scard(textName, function (err, res) {
+    	console.log("number of sets:", res); // will be true if successfull
+	});
 	
 	// client.llen(listName+"set", function(err, len){
 	// 	var totalItems = len
